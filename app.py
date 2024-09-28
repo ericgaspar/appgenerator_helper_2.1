c4abcb24ac1500aada20dcd54f21efff297287fa
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-09-28 16:33:37 +0200

    cleaning

--- app.py
+++ app.py
@@ -555,7 +555,7 @@ class MoreAdvanced(FlaskForm):
     # TODO : specify custom log file
     # custom_log_file = "/var/log/$app/$app.log" "/var/log/nginx/${domain}-error.log"
     use_fail2ban = BooleanField(
-        _("Protect the application against brute force attacks (via fail2ban)"),
+        _("Protect the application against brute force attacks (via Fail2Ban)"),
         default=False,
         render_kw={
             "title": _(
